6f569fbfc729b213b1be68fc6b48390b52cbc634
safahasnaoui22 <safadkhili22@gmail.com>
2026-08-14 13:27:21 +0100

    update installapp

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link href="https://fonts.googleapis.com/css2?family=Henny+Penny&display=swap" rel="stylesheet"></link>
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <link rel="manifest" href="/manifest.json" />
+        <meta
+  name="apple-mobile-web-app-title"
+  content="Primary School"
+/>
         <meta name="theme-color" content="#4F46E5" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
