e783334b040ca8a241845286cd8ae4afff338fb3
safahasnaoui22 <safadkhili22@gmail.com>
2026-08-14 12:21:43 +0100

    updated code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import RegisterSW from './register-sw';
+
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
@@ -36,7 +36,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body> 
         
-     <RegisterSW />
+  
   <Providers>{children}</Providers>
           
           </body>
